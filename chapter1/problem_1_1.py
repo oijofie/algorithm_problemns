2f75fd47136b3c3e54a883f87d44351630d2fff2
oijofie <oijofie@gmail.com>
2022-09-15 00:32:55 +0900

    solved 1.1

--- chapter1/problem_1_1.py
+++ chapter1/problem_1_1.py
@@ -4,13 +4,12 @@ def is_unique(string):
     str_list = []
     for char in string:
         if char in str_list:
-            print(char)
             return False
         str_list.append(char)
     return True
 
 str1 = 'ABC'
-str2 = "abc"
+str2 = "abcc"
 
 print(is_unique(str1))
 print(is_unique(str2))
